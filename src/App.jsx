b13b224f540a47f3b044d98e79ff2ddd6475ba34
gastonlopezt <gaston_lopeztisera@hotmail.com>
2022-04-28 10:20:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import IniciarSesion from './layout/IniciarSesion'
+import Layout from './layout/Layout';
+import Inicio from './pages/Inicio';
+import LoginForm from './pages/LoginForm';
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IniciarSesion />} >
+          <Route index element={<LoginForm />} />
+        </Route>
+        <Route path="/clientes" element={<Layout />}>
+          <Route index element={<Inicio />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
