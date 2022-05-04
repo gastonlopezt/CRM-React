d93110570ee9db63936d336b8cc4f91226a0b7f7
gastonlopezt <gaston_lopeztisera@hotmail.com>
2022-05-04 14:59:12 -0300

    funcionalidad editar cliente

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,8 +3,9 @@ import { useNavigate } from 'react-router-dom'
 import {Formik, Form, Field} from 'formik'
 import * as Yup from 'yup'
 import Alerta from './Alerta';
+import Spinner from './Spinner'
 
-const Formulario = () => {
+const Formulario = ({cliente, cargando}) => {
 
   const navigate = useNavigate()
 
@@ -47,110 +48,118 @@ const Formulario = () => {
   }
 
   return (
-    <div className="bg-white mt-10 px-5 py-10 rouded-md shadow-md md:w-3/4 mx-auto">
-      <h1 className="text-gray-600 font-bold text-xl uppercase text-center">
-        Agregar Cliente
-      </h1>
+    cargando ? <Spinner /> : (
+      <div className="bg-white mt-10 px-5 py-10 rouded-md shadow-md md:w-3/4 mx-auto">
+        <h1 className="text-gray-600 font-bold text-xl uppercase text-center">
+          {cliente?.nombre ? "Editar Cliente" : "Agregar Cliente"}
+        </h1>
 
-      <Formik
-        initialValues={{
-          nombre: "",
-          empresa: "",
-          email: "",
-          telefono: "",
-          notas: "",
-        }}
-        onSubmit={ async (values, {resetForm}) => {
-          await handleSubmit(values)
-          resetForm()
-        }}
-        validationSchema={nuevoClienteSchema}
-      >
-        {({ errors, touched }) => (
-          <Form className="mt-10 ">
-            <div className="mb-4 ">
-              <label className="text-gray-800 " htmlFor="nombre">
-                Nombre
-              </label>
-              <Field
-                id="nombre"
-                type="text"
-                className="mt-2 block w-full p-3 bg-gray-200"
-                placeholder="nombre cliente"
-                name="nombre"
+        <Formik
+          initialValues={{
+            nombre: cliente.nombre,
+            empresa: cliente?.empresa ?? "",
+            email: cliente?.email ?? "",
+            telefono: cliente?.telefono ?? "",
+            notas: cliente?.notas ?? "",
+          }}
+          enableReinitialize={true}
+          onSubmit={async (values, { resetForm }) => {
+            await handleSubmit(values);
+            resetForm();
+          }}
+          validationSchema={nuevoClienteSchema}
+        >
+          {({ errors, touched }) => (
+            <Form className="mt-10 ">
+              <div className="mb-4 ">
+                <label className="text-gray-800 " htmlFor="nombre">
+                  Nombre
+                </label>
+                <Field
+                  id="nombre"
+                  type="text"
+                  className="mt-2 block w-full p-3 bg-gray-200"
+                  placeholder="nombre cliente"
+                  name="nombre"
+                />
+                {errors.nombre && touched.nombre ? (
+                  <Alerta>{errors.nombre}</Alerta>
+                ) : null}
+              </div>
+              <div className="mb-4 ">
+                <label className="text-gray-800 " htmlFor="empresa">
+                  Empresa
+                </label>
+                <Field
+                  id="empresa"
+                  type="text"
+                  className="mt-2 block w-full p-3 bg-gray-200"
+                  placeholder="Empresa del cliente"
+                  name="empresa"
+                />
+                {errors.empresa && touched.empresa ? (
+                  <Alerta>{errors.empresa}</Alerta>
+                ) : null}
+              </div>
+              <div className="mb-4 ">
+                <label className="text-gray-800 " htmlFor="email">
+                  E-mail
+                </label>
+                <Field
+                  id="email"
+                  type="email"
+                  className="mt-2 block w-full p-3 bg-gray-200"
+                  placeholder="Email del cliente"
+                  name="email"
+                />
+                {errors.email && touched.email ? (
+                  <Alerta>{errors.email}</Alerta>
+                ) : null}
+              </div>
+              <div className="mb-4 ">
+                <label className="text-gray-800 " htmlFor="telefono">
+                  Teléfono
+                </label>
+                <Field
+                  id="telefono"
+                  type="tel"
+                  className="mt-2 block w-full p-3 bg-gray-200"
+                  placeholder="Teléfono del cliente"
+                  name="telefono"
+                />
+                {errors.telefono && touched.telefono ? (
+                  <Alerta>{errors.telefono}</Alerta>
+                ) : null}
+              </div>
+              <div className="mb-4 ">
+                <label className="text-gray-800 " htmlFor="notas">
+                  Notas
+                </label>
+                <Field
+                  as="textarea"
+                  id="notas"
+                  type="text"
+                  className="mt-2 block w-full p-3 bg-gray-200 h-40"
+                  placeholder="Notas del cliente"
+                  name="notas"
+                />
+              </div>
+              <input
+                type="submit"
+                value={cliente?.nombre ? "Editar Cliente" : "Agregar Cliente"}
+                className="mt-5 w-full p-3 bg-blue-800 text-white uppercase font-bold text-lg"
               />
-              {errors.nombre && touched.nombre ? (
-                <Alerta>{errors.nombre}</Alerta>
-              ) : null}
-            </div>
-            <div className="mb-4 ">
-              <label className="text-gray-800 " htmlFor="empresa">
-                Empresa
-              </label>
-              <Field
-                id="empresa"
-                type="text"
-                className="mt-2 block w-full p-3 bg-gray-200"
-                placeholder="Empresa del cliente"
-                name="empresa"
-              />
-              {errors.empresa && touched.empresa ? (
-                <Alerta>{errors.empresa}</Alerta>
-              ) : null}
-            </div>
-            <div className="mb-4 ">
-              <label className="text-gray-800 " htmlFor="email">
-                E-mail
-              </label>
-              <Field
-                id="email"
-                type="email"
-                className="mt-2 block w-full p-3 bg-gray-200"
-                placeholder="Email del cliente"
-                name="email"
-              />
-              {errors.email && touched.email ? (
-                <Alerta>{errors.email}</Alerta>
-              ) : null}
-            </div>
-            <div className="mb-4 ">
-              <label className="text-gray-800 " htmlFor="telefono">
-                Teléfono
-              </label>
-              <Field
-                id="telefono"
-                type="tel"
-                className="mt-2 block w-full p-3 bg-gray-200"
-                placeholder="Teléfono del cliente"
-                name="telefono"
-              />
-              {errors.telefono && touched.telefono ? (
-                <Alerta>{errors.telefono}</Alerta>
-              ) : null}
-            </div>
-            <div className="mb-4 ">
-              <label className="text-gray-800 " htmlFor="notas">
-                Notas
-              </label>
-              <Field
-                as="textarea"
-                id="notas"
-                type="text"
-                className="mt-2 block w-full p-3 bg-gray-200 h-40"
-                placeholder="Notas del cliente"
-                name="notas"
-              />
-            </div>
-            <input
-              type="submit"
-              value="Agregar Cliente"
-              className="mt-5 w-full p-3 bg-blue-800 text-white uppercase font-bold text-lg"
-            />
-          </Form>
-        )}
-      </Formik>
-    </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    )
   );
+}
+
+Formulario.defaultProps = {
+  cliente: {},
+  cargando: false
 }
 
 export default Formulario
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Formulario from '../components/Formulario'
 
 const NuevoCliente = () => {
